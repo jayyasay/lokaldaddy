32556e8096101425d8e4caf68fa4cb19c2ff0f3b
unknown <paolo_yasay@yahoo.com>
2023-11-13 10:18:20 +0400

    Test form

--- src/Components/ContactComponents/FormBox.jsx
+++ src/Components/ContactComponents/FormBox.jsx
@@ -4,6 +4,17 @@ import { Container, Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
 const FormBox = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(this);
+    fetch("/api/sendEmail", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  };
   return (
     <>
       <Container className="flex-column" style={{ height: "100%" }}>
@@ -14,22 +25,14 @@ const FormBox = () => {
               <Row className="contact-item-form">
                 <Col xs={12} md={6}>
                   <Form.Label>Name</Form.Label>
-                  <Form.Control placeholder="Name" />
+                  <Form.Control placeholder="Name" name="name" />
                 </Col>
                 <Col xs={12} md={6}>
                   <Form.Label>Contact number</Form.Label>
-                  <Form.Control placeholder="Contact number" />
-                </Col>
-                <Col xs={12} md={6}>
-                  <Form.Label>Date</Form.Label>
-                  <Form.Control placeholder="Date" />
-                </Col>
-                <Col xs={12} md={6}>
-                  <Form.Label>Time</Form.Label>
-                  <Form.Control placeholder="Time" />
+                  <Form.Control placeholder="Contact number" name="number" />
                 </Col>
                 <Col xs={12} md={12}>
-                  <Button type="submit" className="gallery-button">
+                  <Button type="submit" className="gallery-button" onClick={handleSubmit}>
                     Submit
                   </Button>
                 </Col>
