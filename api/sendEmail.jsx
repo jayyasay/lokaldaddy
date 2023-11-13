eb544017d9f1b3612103644a886ed699efff3bcd
unknown <paolo_yasay@yahoo.com>
2023-11-13 10:28:43 +0400

    testing simple api

--- api/sendEmail.jsx
+++ api/sendEmail.jsx
@@ -1,22 +1,3 @@
-const nodeMailer = require("nodemailer");
-
-module.exports = async (req, res) => {
-  const { name, number } = req.body;
-
-  const transporter = nodeMailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "paolo_yasay@yahoo.com",
-      pass: "100908Nanie",
-    },
-  });
-
-  // verify connection configuration
-  transporter.verify(function (error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });
+module.exports = (req, res) => {
+  res.status(200).send("Hello, World!");
 };
