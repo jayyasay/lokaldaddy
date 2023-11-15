a153357336672434f3b0870b8a416ba53c42cf34
unknown <paolo_yasay@yahoo.com>
2023-11-15 17:48:18 +0400

    Added animation for gallery

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,4 +1,4 @@
-import Container from "react-bootstrap/Container";
+import { useState, useEffect } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
@@ -8,7 +8,30 @@ import Gallery3 from "../assets/gallery-3.jpg";
 import { motion } from "framer-motion";
 
 const Gallery = () => {
+  const [images, setImages] = useState([]);
+  const [visibleImages, setVisibleImages] = useState(0);
+
+  useEffect(() => {
+    const loadImages = async () => {
+      const imageModules = import.meta.glob("../assets/gallery/gallery-*.jpg");
+      const imagePaths = await Promise.all(
+        Object.entries(imageModules).map(async ([, importer]) => {
+          const module = await importer();
+          return module.default;
+        })
+      );
+      setImages(imagePaths);
+    };
+
+    loadImages();
+  }, []);
   const pageVariants = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
     initial: {
       opacity: 0,
       y: 20,
@@ -23,31 +46,55 @@ const Gallery = () => {
       transition: { duration: 0.3 },
     },
   };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  };
+
+  const handleViewMore = () => {
+    setVisibleImages((prev) => prev + 3);
+  };
   return (
     <>
       <motion.div
-        className="container-fluid gallery"
+        className="container gallery"
         initial="initial"
         animate="in"
         exit="out"
         variants={pageVariants}
       >
         <h2>Art & Peace</h2>
-        <Row className="gallery-row g-0">
-          <Col xs={12} md={4}>
-            <img src={Gallery1} alt="" className="hover-zoom" />
+        <Row className="gallery-row g-3">
+          <Col md={4} xs={12}>
+            <img src={Gallery1} alt="" className="hover-zoom" loading="lazy" />
           </Col>
-          <Col xs={12} md={4}>
-            <img src={Gallery2} alt="" className="hover-zoom" />
+          <Col md={4} xs={12}>
+            <img src={Gallery2} alt="" className="hover-zoom" loading="lazy" />
           </Col>
-          <Col xs={12} md={4}>
-            <img src={Gallery3} alt="" className="hover-zoom" />
+          <Col md={4} xs={12}>
+            <img src={Gallery3} alt="" className="hover-zoom" loading="lazy" />
           </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <Button className="gallery-button">View Gallery</Button>
-          </Col>
+          {images.slice(0, visibleImages).map((src, index) => (
+            <Col md={4} xs={12}>
+              <motion.img
+                src={src}
+                alt=""
+                className="hover-zoom"
+                loading="lazy"
+                initial="hidden"
+                animate="show"
+                variants={itemVariants}
+              />
+            </Col>
+          ))}
+          {visibleImages < images.length && (
+            <Col xs={12}>
+              <Button className="gallery-button" onClick={handleViewMore}>
+                View more
+              </Button>
+            </Col>
+          )}
         </Row>
       </motion.div>
     </>
